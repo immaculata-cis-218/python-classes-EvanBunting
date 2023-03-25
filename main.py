f8345f38fcbcbadedae6ea052e9121b5a2c48c4e
Evan Bunting <ebunting1@mail.immaculata.edu>
2023-03-25 12:57:26 -0400

    Lab 9

--- main.py
+++ main.py
@@ -17,13 +17,66 @@ class Movie:
         """Calculates a movies rotten tomatoes score"""
         score = self.positive_reviews / self.total_reviews
         return score
-    
-    def get_reviews(self):
+
+    def give_summary(self):
         '''Tells you what movie picked'''
-        return f"You chose the movie {self.name}, which is a score of {self.movie_score()} "
+        return f"You chose the movie {self.name}, which has a score of {self.movie_score()} "
+
+class Runtime(Movie):
+    """The runtime of any movie"""
+    def __init__(self, name, runtime):
+        super().__init__(name, positive_reviews=0, total_reviews=250)
+        self.runtime = runtime
+
+    def calculate_runtime(self):
+        """Calculates the movie's runtime"""
+        hours = self.runtime // 60
+        minutes = self.runtime % 60
+        return f"{hours} hours {minutes} minutes"
+
+    def give_summary(self):
+        """Gives summary for run time"""
+        summary = super().give_summary()
+        summary = f"{summary}, it is {self.runtime} long"
+        return summary
+
+class BoxOffice(Movie):
+    """The box office of any movie"""
+    def __init__(self, name, box_office):
+        super().__init__(name, positive_reviews=220, total_reviews=250)
+        self.box_office = box_office
+
+    def calculate_box_office(self):
+        """Calculates the movie's box office"""
+        return f"${self.box_office:,}"
+
+    def give_summary(self):
+        """Gives summary for box office"""
+        summary = super().give_summary()
+        summary = f"{summary}, and it made {self.box_office} "
+        return summary
 
 if __name__ == "__main__":
     Titanic = Movie("Titanic", 220, 250)
-    Avatar = Movie("Avatar", 247, 335)
+    Avatar = Movie("Avatar", 274, 335)
     print(Titanic.movie_score())
-    print(Titanic.get_reviews())
+    print(Titanic.give_summary())
+
+    Titanic = Runtime("Titanic", 194)
+    print(Titanic.calculate_runtime())
+    print(Titanic.give_summary())
+
+    Titanic = BoxOffice("Titanic", 2187463944)
+    print(Titanic.calculate_box_office())
+    print(Titanic.give_summary())
+
+    print(Avatar.movie_score())
+    print(Avatar.give_summary())
+
+    Avatar = Runtime("Avatar", 162)
+    print(Avatar.calculate_runtime())
+    print(Avatar.give_summary())
+
+    Avatar = BoxOffice("Avatar", 2923706026)
+    print(Avatar.calculate_box_office())
+    print(Avatar.give_summary())
--- test_main.py
+++ test_main.py
@@ -1,4 +1,4 @@
-from main import Movie
+from main import Movie, Runtime, BoxOffice
 
 def test_movie_score():
     test_score = Movie("Titanic", 220, 250)
@@ -9,3 +9,23 @@ def test_movie_score2():
     test_score = Movie("Avatar", 274, 335)
     summary = test_score.movie_score()
     assert .81 < summary < .82
+
+def test_movie_run_time():
+    test_run_time = Runtime("Titanic", 194)
+    summary = test_run_time.calculate_runtime()
+    assert '3 hours 14 minutes' in summary
+
+def test_movie_run_time2():
+    test_run_time = Runtime("Avatar", 162)
+    summary = test_run_time.calculate_runtime()
+    assert '2 hours 42 minutes' in summary
+
+def test_movie_box_office():
+    test_box_office = BoxOffice("Titanic", 2187463944)
+    summary = test_box_office.calculate_box_office()
+    assert '2,187,463,944' in summary
+
+def test_movie_box_office2():
+    test_box_office = BoxOffice("Avatar", 2923706026)
+    summary = test_box_office.calculate_box_office()
+    assert '2,923,706,026' in summary
